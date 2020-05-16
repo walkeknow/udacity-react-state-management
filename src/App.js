87ed18b5b497b134e0d40e5f3b56f5acd94f6ab5
walkeknow <kewalshah077@gmail.com>
2020-05-16 21:42:56 +0530

    Complete task

--- src/App.js
+++ src/App.js
@@ -3,18 +3,29 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  state = {
+    displayText: ''
+  };
+
+  mirrorInput = (query) => {
+    this.setState(() => ({
+      displayText: query
+    }))
+  }
+
   render() {
+    const { displayText } = this.state
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">ReactND - Coding Practice</h1>
         </header>
-        <div className="container">
-          <input type="text" placeholder="Say Something" />
-          <p className="echo">Echo:</p>
-          <p>This should mirror the text you typed into the input field.</p>
-        </div>
+        <input
+          onChange={(event) => { this.mirrorInput(event.target.value) }}
+          type="text">
+        </input>
+        {displayText.length !== 0 && <p>{this.state.displayText}</p>}
       </div>
     );
   }
